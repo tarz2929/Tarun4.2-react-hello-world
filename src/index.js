811d07dd1612083ea04681f6f8cb73313797b4b6
Warren Uhrich <hello@warren.codes>
2020-09-10 11:40:06 -0600

    Update HelloStudent props; output via index.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Hello from './components/Hello'; // If we want to use a component in this file, we have to import it!
+import HelloStudent from './components/HelloStudent';
 
 ReactDOM.render(
   <React.StrictMode>
     <Hello />
+    <HelloStudent name="Birm" unit="React" />
+    <HelloStudent name="Danish" unit="JavaScript" />
+    <HelloStudent name="Adolf" unit="React" />
   </React.StrictMode>,
   document.getElementById('root')
 );
