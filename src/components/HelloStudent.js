c378d06e36317dbfdcd164808c20e12e30df0645
Warren Uhrich <hello@warren.codes>
2020-09-10 11:27:44 -0600

    Add HelloStudent component with prop.

---
+++ src/components/HelloStudent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// We can try a fat arrow function as well (let's try to be consistent in the real world though!)
+// If the component accepts a parameter, this is referred to as a "prop."
+// Props allow us to pass values INTO our components from a parent document / component.
+const HelloStudent = ( name ) => {
+  return (
+    <p>
+      Hello 
+      { name },
+      welcome to TECHCareers and its React unit!
+    </p>
+  );
+}
+
+export default HelloStudent;
