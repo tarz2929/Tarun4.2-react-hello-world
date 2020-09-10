4d9dd5bc3d72edce89117fd98ca9848f7e96d592
Warren Uhrich <hello@warren.codes>
2020-09-10 11:08:56 -0600

    Add Hello component to output.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Hello from './components/Hello'; // If we want to use a component in this file, we have to import it!
 
 ReactDOM.render(
   <React.StrictMode>
-    <></>
+    <Hello />
   </React.StrictMode>,
   document.getElementById('root')
 );
